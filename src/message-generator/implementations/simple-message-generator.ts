e54650c9a73ac376bb5902da8cb8b61204d9cfef
VanyaMate <buhu653@mail.ru>
2024-01-18 08:36:33 +0300

    final

--- src/message-generator/implementations/simple-message-generator.ts
+++ src/message-generator/implementations/simple-message-generator.ts
@@ -7,10 +7,10 @@ export class SimpleMessageGenerator implements IMessageGenerator {
 Repository: [${ props.repositoryOwner }/${ props.repositoryName }](https://github.com/${ props.repositoryOwner }/${ props.repositoryName })
 Status: *${ props.success ? '✅ success' : '🔴 error' }*
 Action: [Link](https://github.com/${ props.repositoryOwner }/${ props.repositoryName }/actions/runs/${ props.actionId })
-Author: [*${ props.commiterUserName }*](https://github.com/${ props.commiterUserName })
+Author: [${ props.commiterUserName }](https://github.com/${ props.commiterUserName })
 Branch: *${ props.branch }*
 Title: *${ props.commitTitle }*
-Commit: [*${ props.commitId }*](https://github.com/${ props.repositoryOwner }/${ props.repositoryName }/commit/${ props.commitId })
+Commit: [${ props.commitId }](https://github.com/${ props.repositoryOwner }/${ props.repositoryName }/commit/${ props.commitId })
 Compare: [Link](${ props.compareUrl })
 Date: *${ props.date }*
         `;
