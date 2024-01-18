7113114fa8ee17b4662e8189bad3bd84da20bb61
VanyaMate <buhu653@mail.ru>
2024-01-18 06:46:32 +0300

    add release to package

--- dist/constants/inputs.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TELEGRAM_CHAT_ID = exports.TELEGRAM_BOT_TOKEN = exports.SUCCESS = void 0;
-exports.SUCCESS = 'success';
-exports.TELEGRAM_BOT_TOKEN = 'tg_bot_token';
-exports.TELEGRAM_CHAT_ID = 'tg_chat_id';
