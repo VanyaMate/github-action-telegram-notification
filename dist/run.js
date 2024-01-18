3fd0ad2898116bcdb74d4f8aea9860b45281b1fd
VanyaMate <buhu653@mail.ru>
2024-01-18 07:36:50 +0300

    upda

--- dist/run.js
+++ dist/run.js
@@ -1,26 +1,48 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    function adopt(value) {
+        return value instanceof P ? value : new P(function (resolve) {
+            resolve(value);
+        });
+    }
+
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        function fulfilled(value) {
+            try {
+                step(generator.next(value));
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        function rejected(value) {
+            try {
+                step(generator["throw"](value));
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        function step(result) {
+            result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+        }
+
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.run = void 0;
+Object.defineProperty(exports, "__esModule", {value: true});
 const core_1 = require("@actions/core");
 const github_1 = require("@actions/github");
 const inputs_1 = require("./constants/inputs");
 const telegram_notification_1 = require("./notificator/implementations/telegram-notification");
 const simple_message_generator_1 = require("./message-generator/implementations/simple-message-generator");
+
 function run() {
     var _a, _b, _c;
     return __awaiter(this, void 0, void 0, function* () {
         try {
             // Data
-            const success = (0, core_1.getInput)(inputs_1.SUCCESS, { required: true });
+            const success = (0, core_1.getInput)(inputs_1.SUCCESS, {required: true});
             const date = new Date().toLocaleString();
             const author = github_1.context.actor;
             const repository = (_b = (_a = github_1.context.payload.repository) === null || _a === void 0 ? void 0 : _a.full_name) !== null && _b !== void 0 ? _b : '';
@@ -39,8 +61,8 @@ function run() {
             console.log('branch ' + branch.toString());
             console.log('commit ' + commit.toString());
             // TG Data
-            const tgBotToken = (0, core_1.getInput)(inputs_1.TELEGRAM_BOT_TOKEN, { required: true });
-            const tgChatId = (0, core_1.getInput)(inputs_1.TELEGRAM_CHAT_ID, { required: true });
+            const tgBotToken = (0, core_1.getInput)(inputs_1.TELEGRAM_BOT_TOKEN, {required: true});
+            const tgChatId = (0, core_1.getInput)(inputs_1.TELEGRAM_CHAT_ID, {required: true});
             if (!tgBotToken) {
                 throw 'Telegram bot token not installed';
             }
@@ -56,11 +78,11 @@ function run() {
             console.log('message ' + message);
             yield notification.notify(tgChatId, message);
             (0, core_1.setOutput)('Notified', true);
-        }
-        catch (error) {
+        } catch (error) {
             if (error instanceof Error)
                 (0, core_1.setFailed)(error.message);
         }
     });
 }
+
 exports.run = run;
